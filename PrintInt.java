91daaef0c4cb99172ac74526d5bc99499ace4127
Aashish-kushwaha <kushwahaashu444@gmail.com>
2023-02-21 09:20:38 +0530

    print integer values

---
+++ PrintInt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class PrintInt {
+    public static void main(String k[])
+    {
+        int c;
+        for(c=1;c<=10;c++)
+        {
+            System.out.println(c);
+        }
+    }
+    
+}
--- simple.java
+++ simple.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 class Simple{
     public static void main(String k[])
     {
