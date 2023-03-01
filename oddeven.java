757f9241fa5103abfc4a730d0a032eff928eeba9
Aashish-kushwaha <kushwahaashu444@gmail.com>
2023-03-01 23:06:38 +0530

    10th program in java

---
+++ oddeven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Oddeven {
+    public static void main(String k[])
+    {
+        int a;
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        if(a%2==0)
+        {
+            System.out.println(a+"is even");
+        }else
+        {
+            
+            System.out.println(a+"is odds");
+        }
+    }   
+}
