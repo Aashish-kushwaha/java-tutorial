b5512ee55c929fc3e07ac8ca36fa45f2b6c7e201
Aashish-kushwaha <kushwahaashu444@gmail.com>
2025-01-21 23:33:53 +0530

    multiple table

---
+++ logic building question/multiplication_table.java
@@ -0,0 +1,11 @@
+public class multiplication_table {
+
+    public static void main(String k[])
+    {
+        int n=2;
+        for(int i=1;i<=10;i++)
+        {
+            System.err.println(n*i);
+        }
+    }
+}
